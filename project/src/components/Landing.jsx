b9bdc1865fa1e798ae8e49d5f2a995f6a8d54e13
thashin04 <thashin2004@gmail.com>
2024-11-10 20:17:16 -0500

    no more place holder

--- project/src/components/Landing.jsx
+++ project/src/components/Landing.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../index.css';
 import landing from '../images/landing.png';
-import placeholder from '../images/placeholder.png';
 import landvid from '../images/landvid.mp4';
 
 const Landing = () => {
