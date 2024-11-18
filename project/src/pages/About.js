149195a37ce3da66b2684897760cfcc391ebf9b9
LeekOnionRings <156482704+LeekOnionRings@users.noreply.github.com>
2024-11-18 01:02:12 -0500

    Literally Just Testing
    Can I make it work?

--- project/src/pages/About.js
+++ project/src/pages/About.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import '../index.css'
+import Banner from '../components/Banner'
+
+function About() {
+    return (
+        <div>
+            <Banner />
+        </div>
+    );
+}
 
 export default About;
