897c478fda51525697a822ef13f6525cc5e61b3c
alaskasalad <caitlin.boxx@gmail.com>
2024-11-18 23:15:56 -0500

    updated footer.jsx, link fb insta email about products. took out contact us under our products

--- project/src/components/Footer.jsx
+++ project/src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import {
   FaFacebookSquare,
@@ -14,22 +16,39 @@ const Footer = () => {
       <div className='lg:col-span-2 flex flex-col md:flex-row justify-between'>
         <div className='mb-6 md:mb-0'>
           <ul>
-            <li className='py-2 text-md'>About Us</li>
-            <li className='py-2 text-md'>Our Products</li>
-            <li className='py-2 text-md'>Contact</li>
+
+          <Link to="/About" className="hover:underline text-white-600">
+                About Us
+            </Link>
+            <br></br>
+            <Link to="/Products" className="hover:underline text-white-600">
+                Our Products
+            </Link>
             <li className='py-2 text-md'>Book an Appointment</li>
+
           </ul> 
         </div>
         <div>
           <div className='flex items-center space-x-4 mb-6 md:mb-0'>
             <h6 className='font-medium pr-8'>Follow us:</h6>
             {/* Icons side by side next to heading */}
-            <FaFacebookSquare size={30} />
-            <FaInstagram size={30} />
+            <a href="https://m.facebook.com/capistranodistillery/" target="_blank" rel="noopener noreferrer">
+              <FaFacebookSquare size={30} />
+              </a>
+              <a href="https://www.instagram.com/capistranodistillery?igsh=MTM1MGM1YXRiZ2F2ZQ==" target="_blank" rel="noopener noreferrer">
+              <FaInstagram size={30} />
+              </a>
           </div>
           <ul className='mt-4 text-right max-md:text-left'>
-            <li>For inquiries, please contact:</li>
-            <li>email@email.com</li>
+          <li>For inquiries, please contact:</li>
+            <li>
+              <a
+                href="mailto:capistrano_distillery@yahoo.com"
+                className="text-white hover:teext-blue-600 underline"
+              >
+              capistrano_distillery@yahoo.com
+              </a>
+            </li>
           </ul>
         </div>
       </div>
